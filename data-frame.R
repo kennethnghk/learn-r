df026c389ed433ce0dc4483c808d7b743c4b51a6
kenrefu <ng.tszcheong@gmail.com>
2016-10-04 11:40:15 +0800

    Add data-frame.R

---
+++ data-frame.R
@@ -0,0 +1,7 @@
+# load objects in this file: source('XXX')
+# list all objects in workspace : ls()
+# remove object : rm()
+
+teamData <- data.frame(team = c('Arsenal', 'Man U', 'Man City'), score = c(45,34,33))
+
+# nrow(), ncol()
