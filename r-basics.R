bdc648b051513c6f571403919d8fac3b8c4bdd11
kenrefu <ng.tszcheong@gmail.com>
2016-10-04 11:52:48 +0800

    update r-basics

---
+++ r-basics.R
@@ -0,0 +1,17 @@
+# load objects in this file: source('XXX')
+# list all objects in workspace : ls()
+# remove object : rm()
+
+#### vector / matrix / list
+vector1 <- 1:3
+names(vector1) <- c('name1', 'name2', 'name3')
+
+list1 <- list(a = 1, b = 2, c = 3)
+
+matrix1 <- matrix(1:6, nrow=2, ncol=3)
+dimnames(matrix1) <- list(c("row1", "row2"), c("col1", "col2", "col3"))
+
+#### Data frames
+teamData <- data.frame(team = c('Arsenal', 'Man U', 'Man City'), score = c(45,34,33))
+
+# nrow(), ncol()
