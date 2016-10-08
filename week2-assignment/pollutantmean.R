3345465489aa00f908986811fdef224ea2961931
kenrefu <ng.tszcheong@gmail.com>
2016-10-08 22:27:04 +0800

    fix pollutantmean function

--- week2-assignment/pollutantmean.R
+++ week2-assignment/pollutantmean.R
@@ -1,16 +1,16 @@
 pollutantmean <- function(directory, pollutant, id=1:332) {
     
-    allMeans <- c()
+    allData <- c()
     
     for (i in id) {
         fileName <- file.path(directory, paste(sprintf("%.3d", i), '.csv', sep=''))
         statData <- read.csv(fileName)
         pollutantColData <- statData[[pollutant]]
-        naValues <- is.na(pollutantColData)
-        pollutantColData <- pollutantColData[!naValues]
-        fileMean <- mean(pollutantColData)
-        allMeans <- append(allMeans, fileMean)
+        #naValues <- is.na(pollutantColData)
+        #pollutantColData <- pollutantColData[!naValues]
+        #fileMean <- mean(pollutantColData, na.rm = TRUE)
+        allData <- append(allData, pollutantColData)
     }
     
-    mean(allMeans)
+    mean(allData, na.rm = TRUE)
 }
