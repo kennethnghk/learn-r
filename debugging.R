ebffcda6e173fac36621981b798c809b0fa03a98
kenrefu <ng.tszcheong@gmail.com>
2016-10-15 09:39:56 +0800

    add tapply and debugging

---
+++ debugging.R
@@ -0,0 +1,10 @@
+## log levels
+message()
+warning()
+error()
+
+traceback() ## prints out the function call stack (most recent error)
+debug() ## terminate execution when calling that function
+browser ## debug browser
+trace() ## add debugging code
+recover() ## when error occurs, freeze data and print function call stack
--- loop-functions.R
+++ loop-functions.R
@@ -20,3 +20,31 @@ rowSums()
 rowMeans()
 colSums()
 colMeans()
+
+## mapply (can apply to multiple lists)
+list(rep(1,4), rep(2,3), rep(3,2), rep(4,1))
+mapply(rep, 1:4, 4:1) ## can vectorize function inputs which are not allowed vector
+
+## tapply (apply to subset of vector)
+x <- c(1:30)
+f <- gl(3,10) ## define 3 levels, which one contains 10 objects
+tapply(x, f, mean) ## find the mean of level 1, 2, 3 numbers respectively
+
+## split (split vectors / objects into group)
+x <- c(1:30)
+f <- gl(3,10)
+split(x,f) ## split into 3 groups (levels) by f
+lapply(split(x,f), mean) ## same as tapply(x,f,mean)
+
+library(datasets)
+airquality ## airquality data frame
+s <- split(airquality, airquality$Month) ## split into groups by month
+lapply(s, function(x) colMeans(x[, c("Ozone", "Wind")])) ## find the mean Ozone and Wind of each month
+
+## split on more than 1 levels
+x <- c(1:30)
+f1 <- gl(2,5)
+f2 <- gl(5,2)
+interaction(f1,f2)
+split(x, list(f1,f2), drop=TRUE) ## split by interaction(f1,f2), ie. by 2 combinations
+## drop means dropping empty levels
